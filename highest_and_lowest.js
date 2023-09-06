67f4a218edb19f4870873418e4279b941eeb379e
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-09-06 17:53:48 -0600

    Add instructions

---
+++ highest_and_lowest.js
@@ -0,0 +1,4 @@
+// In this little assignment you are given a string of
+// space separated numbers, and have to return the highest
+// and lowest number. Output string must be two numbers
+// separated by a single space, and highest number is first.
