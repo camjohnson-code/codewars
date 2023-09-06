92de1c4e724f5d4c9be8a29673573d5692287222
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-09-06 17:57:37 -0600

    Complete challenge

--- highest_and_lowest.js
+++ highest_and_lowest.js
@@ -2,3 +2,14 @@
 // space separated numbers, and have to return the highest
 // and lowest number. Output string must be two numbers
 // separated by a single space, and highest number is first.
+
+function highAndLow(str){
+    // split string into array and sort 
+    strArray = str.split(' ').sort((a, b) => a - b)
+    
+    // return last index and 0 index
+    console.log(`${strArray[strArray.length -1]} ${strArray[0]}`); 
+}
+
+highAndLow(("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
+highAndLow(("10 2 -1 -20"), "10 -20", "Sorting a collection of strings is different from sorting a collection of numbers");
