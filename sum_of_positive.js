042e555e128905ca08cbc8886fc6b20395e67a5c
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-09-12 20:46:16 -0600

    Add second solution

--- sum_of_positive.js
+++ sum_of_positive.js
@@ -4,24 +4,41 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
+// SOLUTION 1
+// function positiveSum(arr) {
+//   // create new array
+//   var positiveArr = [0];
+
+//   // loop through array
+//   for (i = 0; i < arr.length; i++) {
+//     // check if index is positive
+//     // push positive to new array
+//     if (arr[i] > 0) positiveArr.push(arr[i]);
+//   }
+
+//   //   reduce on positiveArr
+//   var sum = positiveArr.reduce(function (a, b) {
+//     return a + b;
+//   });
+
+//   //   return sum
+//   return sum;
+// }
+
+// SOLUTION 2
 function positiveSum(arr) {
-  // create new array
-  var positiveArr = [0];
+  // set total to 0
+  var total = 0;
 
   // loop through array
   for (i = 0; i < arr.length; i++) {
-    // check if index is positive
-    // push positive to new array
-    if (arr[i] > 0) positiveArr.push(arr[i]);
+    // check if integer is positive
+    // add value to total
+    if (arr[i] > 0) total += arr[i];
   }
 
-  //   reduce on positiveArr
-  var sum = positiveArr.reduce(function (a, b) {
-    return a + b;
-  });
-
-  //   return sum
-  return sum;
+  //   return total
+  return total;
 }
 
 console.log(positiveSum([1, 2, 3, 4, 5]));
