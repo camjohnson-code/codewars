99c5d047c43ac56052844d5f31fc8b0f31198e2a
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-11-22 12:06:45 -0700

    Pass tests

---
+++ build_tower.js
@@ -0,0 +1,36 @@
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors.
+// A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+  const tower = [];
+
+  for (let i = 1; i <= nFloors; i++) {
+    const spaces = ' '.repeat(nFloors - i);
+    const stars = '*'.repeat(2 * i - 1);
+    const floor = spaces + stars + spaces;
+    tower.push(floor);
+  }
+
+  return tower;
+}
+
+console.log(towerBuilder(9));
