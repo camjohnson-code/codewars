3fe3c89f6f9f70fd932e8ad514fc674c2770e3c5
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-05 08:44:57 -0700

    Refactor solution

--- multiples_of_3_or_5.js
+++ multiples_of_3_or_5.js
@@ -6,8 +6,8 @@
 
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
-function solution(number) {
-  var num = 0;
+const solution = (number) => {
+  let num = 0;
 
   if (number < 0) return 0;
   else {
