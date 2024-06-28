e1c24e52e3a13f4933ece2578f2ad9228113b457
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-06-28 08:43:06 -0600

    Pass tests

---
+++ rank_vector.js
@@ -0,0 +1,21 @@
+// Given an array (or list) of scores, return the array of ranks for each value in the array.
+// The largest value has rank 1, the second largest value has rank 2, and so on.
+// Ties should be handled by assigning the same rank to all tied values. For example:
+
+// array = [9,3,6,10] --> ranks = [2,4,3,1]
+
+// array = [3,3,3,3,3,5,1] --> ranks = [2,2,2,2,2,1,7]
+
+// because there is one 1st place value, a five-way tie for 2nd place, and one in 7th place.
+
+const ranks = (a) => {
+  const sorted = a.slice().sort((a, b) => b - a);
+  const output = [];
+  for (let i = 0; i < a.length; i++) {
+    output.push(sorted.indexOf(a[i]) + 1);
+  }
+
+  return output;
+};
+
+console.log(ranks([3, 3, 3, 3, 3, 5, 1]));
